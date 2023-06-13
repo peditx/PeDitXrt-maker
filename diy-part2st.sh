41ed8933f96c11c60ad4d19889029ab546709529
PeDitX <pedram.ale@me.com>
2023-06-13 04:55:33 +0330

    Update diy-part2st.sh

--- diy-part2st.sh
+++ diy-part2st.sh
@@ -9,8 +9,8 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# Modify default IP and hostname
+wget -O package/base-files/files/bin/config_generate https://github.com/peditx/PeDitXrt/blob/35688c9d0d90a2bf2deb3a8549b2040daa1d0a87/package/base-files/files/bin/config_generate
 
 # Download and replace banner file
 wget -O package/base-files/files/etc/banner https://github.com/peditx/PeDitXrt/raw/35688c9d0d90a2bf2deb3a8549b2040daa1d0a87/package/base-files/files/etc/banner
