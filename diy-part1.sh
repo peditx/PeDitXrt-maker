516eac67b0c950aab701dfb050dbd713ceb7112f
PeDitX <pedram.ale@me.com>
2024-01-20 13:55:27 +0330

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,6 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-echo 'src-git luci-theme-design https://github.com/gngpp/luci-theme-design.git' >>feeds.conf.default
+# echo 'src-git luci-theme-design https://github.com/gngpp/luci-theme-design.git' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # echo 'src-git haibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
